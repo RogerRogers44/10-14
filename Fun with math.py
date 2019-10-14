b47cf6d6a14b3c3d7b55a795d8f4f840f22c1229
100168747 <whatsitsname123@gmail.com>
2019-10-14 15:34:51 -0500

    I got a break...

--- Fun with math.py
+++ Fun with math.py
@@ -5,3 +5,19 @@ print(455/113)
 import math
 print(9801/ (2206 * math.sqrt(2)))
 
+
+def archimedes(numSides):
+    innerAngleB = 360.0 / numSides
+    halfAngleA = innerAngleB / 2
+    oneHalfSideS = math.sin(math.radians(halfAngleA))
+    sideS = oneHalfSideS * 2
+    polygonCircumference = numSides * sideS
+    pi = polygonCircumference / 2
+    return pi
+
+
+print(archimedes(4))
+print(archimedes(8))
+print(archimedes(16))
+for sides in range(8, 100, 8):
+    print(sides, archimedes(sides))
